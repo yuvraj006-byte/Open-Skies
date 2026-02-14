afbfc74d3378658af0648ac4e42ae1b20bdb73bd
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-02-14 17:22:25 +0200

    Basic Functions, check these and try to integrated them in your code!

--- Game_name/Basic Functions/Lopping_fucntion/prompt_until.py
+++ Game_name/Basic Functions/Lopping_fucntion/prompt_until.py
@@ -18,3 +18,27 @@ def prompt_until(task):
     return
 prompt_until() # What ever Function you have defined than needs to be looped, call it inside here!
 
+#Example:
+import random
+def roll_die():
+    return print(random.randint(1,6))
+
+def prompt_until(task):
+    retry_variable = "Do You Want To Continue (Y/N)?: "
+    task() # Calls the function for the First time.
+
+    while True:
+        retry_input = input(retry_variable).strip().lower()
+
+        if retry_input == "y":
+            task() # Calling the function for lopping and continues the loop.
+        
+        elif retry_input == "n":
+            print("Good Bye!\nSee You Soon.")
+            break
+        
+        else:
+            print("Invalid Input! Please Try Again!")
+            # Continues the loop if Invalid Input
+    return
+prompt_until(roll_die) # What ever Function you have defined than needs to be looped, call it inside here!
