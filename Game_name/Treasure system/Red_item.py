61864ede27b86d51d425a6978658c9a020993645
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-02-16 13:29:22 +0200

    Red Items - in progress

--- Game_name/Treasure system/Red_item.py
+++ Game_name/Treasure system/Red_item.py
@@ -5,7 +5,7 @@ def get_query():
     connection = mysql.connector.connect(
         host = "localhost",
         user = "user",
-        password = "password",
+        password = "your_password",
         database = "game_project",
     )
     return connection
@@ -63,10 +63,42 @@ def get_random_item():
 # ---- Usage ----
 item = get_random_item()
 
-if item:
-    print(item["name"])
-    print(item["type"])
-    print(item["rarity"])
-    print(item["skills"])
+class RedItems:
+    def __init__(self, damage_dealt, armor_strength, shield_strength, healing_done, name, type, rarity, skills ):
+        self.damage_dealt = damage_dealt,
+        self.armor_strength = armor_strength,
+        self.shield_strength = shield_strength,
+        self.healing_done = healing_done,
+        self.name = name,
+        self.type = type,
+        self.rarity = rarity,
+        self.skills = skills
+
+    def __str__(self):
+        return f"NAME: {self.name}, TYP: {self.type},RARITY:{self.rarity}, SKL: {self.skills}\nDMG : {self.damage_dealt}, AR: {self.armor_strength}, SR: {self.shield_strength}, HEAL: {self.healing_done}"
 
 
+class RedWeapons(RedItems):
+    @classmethod
+    def create(cls):
+        red_item_points = random.randint(24, 35)
+        red_item_multiplier = 2.6
+        if item:
+            red_item_name = (item["name"])
+            red_item_type = (item["type"])
+            red_item_rarity = (item["rarity"])
+            red_item_skills = (item["skills"])
+        return cls (
+            name = red_item_name,
+            type = red_item_type,
+            rarity = red_item_rarity,
+            skills = red_item_skills,
+            damage_dealt = red_item_points * red_item_multiplier,
+            armor_strength = red_item_points * red_item_multiplier,
+            shield_strength = red_item_points * red_item_multiplier,
+            healing_done = red_item_points * red_item_multiplier
+
+        )
+
+redItem = RedWeapons.create()
+print(redItem)
