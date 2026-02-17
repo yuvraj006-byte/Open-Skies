1faabd3c987a3c686a741d1eb6728dc199d7d357
hassanshome <hassan7722@gmail.com>
2026-02-17 12:12:31 +0200

    Loot_function.

--- Game_name/Treasure system/Black_item_funtions.py
+++ Game_name/Treasure system/Black_item_funtions.py
@@ -6,7 +6,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Cat",
+        password="7",
         database="game_project",
     )
 
